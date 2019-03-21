b3ba4cb060f2f262bf8fe9e8d1715d187aabd315
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 06:27:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+speakers = ["Edsger", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
