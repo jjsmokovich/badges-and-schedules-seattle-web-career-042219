7cdab6e87554e22c1203cdf463cb1e4e1f33ada6
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 07:18:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-    speakers.each_with_index.map do |name, room_num|
+    speakers.each_with_index do |name, room_num|
        "Hello, #{name}! You'll be assigned to room #{room_num}."
     end
 end
